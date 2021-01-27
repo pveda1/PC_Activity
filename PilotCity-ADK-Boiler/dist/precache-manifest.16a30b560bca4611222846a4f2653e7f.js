40236d58f2c687011f72dfeb9e81eefe4cb1c51e
pveda1 <parulveda1@gmail.com>
2021-01-26 23:46:13 -0800

    adding the files from the old folder

---
+++ PilotCity-ADK-Boiler/dist/precache-manifest.16a30b560bca4611222846a4f2653e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bae271bb30c9c1cd89",
+    "url": "/css/app.a896eb70.css"
+  },
+  {
+    "revision": "0640fe04a278233a5803",
+    "url": "/css/chunk-vendors.28bbe3b3.css"
+  },
+  {
+    "revision": "55261080d8220cbb13b7a14855e396a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1bae271bb30c9c1cd89",
+    "url": "/js/app.a17dbe3a.js"
+  },
+  {
+    "revision": "0640fe04a278233a5803",
+    "url": "/js/chunk-vendors.90bdd85a.js"
+  },
+  {
+    "revision": "5e7abea6391156883a9eb3ec07a6094e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
